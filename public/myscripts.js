7379e0ff15ad1d7379c87d3ba3a1f0916643e848
Ninad Bhole <ninad.bhole@plansource.com>
2023-08-23 13:15:54 +0530

    added code to replace few words with emojies in message

---
+++ public/myscripts.js
@@ -0,0 +1,42 @@
+$(function () {
+    const socket = io();
+    const $prompt = $('#prompt');
+    const $chatContainer = $('#chat-container');
+    const $nameForm = $('#name-form');
+    const $nameInput = $('#name-input');
+    const $chatContent = $('#chat-content');
+    const $messages = $('#messages');
+    const $form = $('#form');
+    const $input = $('#input');
+    const emojies = {hey: "🤚", lol: "😂", woah: "😮", like: "❤️", congratulations: "🎉", react: "⚛"};
+    let username;
+
+    $nameForm.submit(function() {
+      username = $nameInput.val();
+      if (username) {
+        $prompt.addClass('hidden');
+        $chatContainer.removeClass('hidden');
+      }
+      return false;
+    });
+
+    $form.submit(function() {
+      if ($input.val()) {
+        let message = $input.val();
+        for (const word in emojies) {
+          if (message.includes(word)) {
+            message = message.replace(new RegExp(word, "gi"), emojies[word]);
+          }
+        }
+        socket.emit('chat message', { username, message });
+        $input.val('');
+      }
+      return false;
+    });
+
+    socket.on('chat message', function(data) {
+      $messages.append($('<li>').text(data.username + ": " + data.message));
+      $chatContent.scrollTop($chatContent[0].scrollHeight);
+    });
+
+  });
